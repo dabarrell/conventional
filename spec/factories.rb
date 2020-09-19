b8f6a266a2281b0cd74586ed70fd2ab49f11b081
David Barrell <david@barrell.me>
2020-09-19 19:06:25 +1000

    chore: Add cli/bump spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require "factory_bot"
+require "rubygems/version"
 
 require "conventional/entities/commit"
+require "conventional/entities/tag"
 
 FactoryBot.define do
   factory :commit, class: Conventional::Entities::Commit do
@@ -35,5 +37,15 @@ FactoryBot.define do
     trait :fix do
       type { "fix" }
     end
+  end
+end
+
+FactoryBot.define do
+  factory :tag, class: Conventional::Entities::Tag do
+    initialize_with { new(attributes) }
+
+    value { "v1.0.0" }
+    version { Gem::Version.new(value.delete_prefix(prefix)) }
+    prefix { "v" }
   end
 end
